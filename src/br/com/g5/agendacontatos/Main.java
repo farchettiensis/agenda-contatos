8af60acc71e015ee05455a7295662214cd2cf830
victorferreiras <victor.fagundes586@gmail.com>
2024-09-03 22:33:27 -0300

    tests

--- src/br/com/g5/agendacontatos/Main.java
+++ src/br/com/g5/agendacontatos/Main.java
@@ -1,50 +1,17 @@
 package br.com.g5.agendacontatos;
 
 import br.com.g5.agendacontatos.agenda.Agenda;
+import br.com.g5.agendacontatos.agenda.AgendaController;
 import br.com.g5.agendacontatos.contato.Contato;
 import br.com.g5.agendacontatos.telefone.Telefone;
 import br.com.g5.agendacontatos.telefone.TipoTelefone;
 
 public class Main {
     public static void main(String[] args) {
+        AgendaController controller = new AgendaController();
 
-        //Sistema sistema = new Sistema();
-        //sistema.iniciarSistema();
+        controller.adicionarContato();
 
-        Telefone telefone1 = new Telefone("12345678909", TipoTelefone.CELULAR);
-        Telefone telefone2 = new Telefone("98765432109", TipoTelefone.CASA);
-        Telefone telefone3 = new Telefone("12345678919", TipoTelefone.TRABALHO);
-
-        Contato contato1 = new Contato("Donald Fagen", telefone1, "Rua Custerdome, 1", "dfagen@steely.dan", "deaconBlues");
-        String cont1Detalhes = contato1.detalharContato();
-        System.out.println(cont1Detalhes);
-
-        Contato contato2 = new Contato("Walter Becker", telefone2, "Rua Custerdome, 2", "wbecker@steely.dan", "homeAtLast");
-        Contato contato3 = new Contato("Skunk Baxter", telefone3, "Rua Custerdome, 3", "sbaxter@doobie.bros", "46346585");
-        Contato contato4 = new Contato("Donald Fagen", telefone1, "Rua Custerdome, 4", "dfagen2@steely.dan", "deaconBlues2");
-
-        Filme rato = new Filme("Ratatouille");
-        Filme us = new Filme("Us");
-        Filme corra = new Filme("Corra");
-
-        Agenda agenda = new Agenda();
-        agenda.adicionarContato(contato1);
-        agenda.adicionarContato(contato2);
-        agenda.adicionarContato(contato3);
-        //agenda.adicionarContato(contato4);
-
-        contato1.cadastrarFilme(rato);
-        contato1.cadastrarFilme(us);
-        //contato1.cadastrarFilme(corra);
-        contato1.darNotaFilme(rato,10.00);
-        contato1.detalharContato();
-
-        //System.out.println(agenda.listarTodosOsContatos());
-
-        //agenda.removerContato(contato3);
-        //System.out.println(agenda.listarTodosOsContatos());
-
-        agenda.buscarContato("12345678909");
 
     }
 }
--- src/br/com/g5/agendacontatos/telefone/TelefoneService.java
+++ src/br/com/g5/agendacontatos/telefone/TelefoneService.java
@@ -10,12 +10,12 @@ public class TelefoneService extends Telefone{
         System.out.println("Escreva o seu numero");
         telefone.setNumero(sc.next());
         System.out.println("Escolha o tipo do telefone: \n" +
-                "1 - Celular" +
-                "2 - Casa" +
-                "3 - Trabalho" +
-                "4 - Escola" +
-                "5 - Principal" +
-                "6 - Outro");
+                "\n 1 - Celular" +
+                "\n 2 - Casa" +
+                "\n 3 - Trabalho" +
+                "\n 4 - Escola" +
+                "\n 5 - Principal" +
+                "\n 6 - Outro");
         int opcao = sc.nextInt();
         super.setTipo(opcao);
 

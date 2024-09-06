731246f64fb5cbf17c3c976f90c680674a1328ca
Lothar <lothar223@hotmail.com>
2024-09-06 20:55:22 -0300

    refactor editarContato

--- src/br/com/g5/agendacontatos/agenda/AgendaService.java
+++ src/br/com/g5/agendacontatos/agenda/AgendaService.java
@@ -36,6 +36,9 @@ public class AgendaService extends Agenda{
             if (contato.getTelefone().getNumero().equals(telefone)) {
                 Contato novoContato = contatoService.requisitarContatoNoTerminal();
                 this.contatos.set(contatos.indexOf(contato), novoContato);
+            } else {
+                System.out.println("Contato não encontrado!");
+                return;
             }
         }
     }
